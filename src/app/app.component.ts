9e2b833888a652937e58ec93d12bc10a1fa1c8a3
Krishan Malinda <gampalagekm@gmail.com>
2025-01-31 12:42:24 +0530

    Login and Dashboard Create

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponet } from "./login/login.component";
+import { DashBoardComponent } from "./dash-board/dash-board.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponet, DashBoardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first';
+  title = 'Angular Project';
 }

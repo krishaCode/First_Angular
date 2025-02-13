4091d294f6f08f4e45a3db9484dc514c652cc4d2
Krishan Malinda <gampalagekm@gmail.com>
2025-02-13 10:32:12 +0530

    Data binding 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponet } from "./login/login.component";
 import { DashBoardComponent } from "./dash-board/dash-board.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponet, DashBoardComponent],
+  imports: [RouterOutlet, LoginComponet, DashBoardComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
   title:string = 'Hello World';
+  inputValue:string =''
   imgUrl:string ="C:\Users\KRISHAN\Desktop";
   isDisabled:boolean = false;
   arialabltex="This is button."
@@ -19,6 +20,7 @@ export class AppComponent {
 color:string='red';
 isActive:boolean = false;
 name:string='Krishan'
+twoWayValue:string =''
 
 buttonclicked() {
   console.log("Button clicked")
@@ -29,6 +31,15 @@ if(event.key == "Enter"){
   console.log("Enter clicked")
 }
   }
+
+  KeyDownClick() {
+    console.log("Enter clicked")
+    }
+
+    Krypressed(input:HTMLInputElement) {
+      this.inputValue=input.value
+      console.log(input.id)
+      }
 
 }
 
